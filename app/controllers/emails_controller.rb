4daa2f606f4197465f4f3195d571a2380a56e8b7
joshuanspada@hotmail.fr <joshuanspada@hotmail.fr>
2019-08-29 17:02:37 +0200

    uff

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,34 @@
+class EmailsController < ApplicationController
+
+  def index
+    @emails = Email.all
+  end
+
+  def create
+  @email = Email.create(object: Faker::FunnyName.name, body: Faker::GreekPhilosophers.quote)
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+
+  end
+
+end
+
+  def show
+    @email = Email.find(params[:id])
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+  end
+
+
+  def destroy
+    @email = Email.find(params[:id])
+    @email.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+  end
+end
